ecafec45206c6bc1de34550898c19c69f2f70244
BJSwaroop <BJSwaroop1@gmail.com>
2021-08-31 18:16:05 +0530

    modified bullet prop

--- index.js
+++ index.js
@@ -15,23 +15,9 @@ window.onload = ()=> {
         // console.log("Hello")
     }
     function checkKey(e){
-        const speed = 15;
+        const speed = 8;
         var c = launcher.getBoundingClientRect()
         console.log(e.keyCode)
-        if(e.keyCode == '32'){
-            var newBullet = document.createElement("div");
-        newBullet.classList.add("bullet");
-        newBullet.classList.add("newbullet");
-
-        newBullet.innerHTML = '<img  style= "height: 1rem;" src="bullet.png" alt="">'
-        
-        
-        document.body.firstElementChild.children[1].appendChild(newBullet);
-            shoot();
-            
-
-
-        }
         if(e.keyCode == '38'){
             if(c.top> 523){
                 launcher_translate[1] -= speed;
@@ -53,14 +39,46 @@ window.onload = ()=> {
             }
         
         }
+        if(e.keyCode == '32'){
+            var newBullet = document.createElement("div");
+        newBullet.classList.add("bullet");
+        newBullet.classList.add("newbullet");
+
+        newBullet.innerHTML = '<img  style= "height: 1rem;" src="bullet.png" alt="">'
+        
+        
+        document.body.firstElementChild.children[1].appendChild(newBullet);
+            shoot();
+            
+
+
+        }
+
         
         console.log(launcher_translate)
         launcher.style.transform = `translate(${launcher_translate[0]}px,${launcher_translate[1]}px)`
         
     }
+    collision = function(r1,r2) {
+            return Math.max(r1.top, r2.top) < Math.min(r1.bottom, r2.bottom) && Math.min(r1.right,r2.right) > Math.max(r1.left , r2.left);
+    }
     document.onkeydown = checkKey;
-    var bullet = document.querySelector(".bullet");
+    var bullets = document.querySelectorAll(".bullet");
     var aliens = document.querySelectorAll(".alien"); 
+    setInterval(()=> {
 
+        aliens.forEach((alien) => {
+            var al = alien.getBoundingClientRect()
+            bullets.forEach((bullet) => {
+                var br = bullet.getBoundingClientRect()
+                console.log(br)
+                if(collision(al,br)){
+                    hit(alien);
+                    document.body.firstElementChild.children[1].removeChild(bullet);
+                }
+            });
 
+        });
+    }, 100
+     )
 }
